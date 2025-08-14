4158cf250e7df512c1379325364255abdca542a0
Jakkapan-a <jakkapan.attala@gmail.com>
2025-08-14 16:01:42 +0700

    Update

--- service1/index.js
+++ service1/index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
       <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>My Static Site with Counter</title>
+          <title>My Static Site with Counter 1</title>
       </head>
       <body>
           <h1>Welcome to My Static Site 1</h1>
